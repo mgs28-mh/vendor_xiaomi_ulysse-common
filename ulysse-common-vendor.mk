b4dc8b14e68d69cd09e8dd97074910bf8667777c
mgs28-mh <gsaputra880@gmail.com>
2022-06-01 18:28:33 +0700

    ulysse-common: Import qti-telephony-common.jar from caf
    - from LA.QSSI.12.0.r1-05800-qssi.0
    
    Change-Id: Iddc64bb208ddb05ff1f24e457e27f01474fe52aa

--- ulysse-common-vendor.mk
+++ ulysse-common-vendor.mk
@@ -650,6 +650,7 @@ PRODUCT_PACKAGES += \
     dpmapi \
     embmslibrary \
     qcrilhook \
+    qti-telephony-common \
     android.hardware.gnss@2.1-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
